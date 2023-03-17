8c1a34e1401c3d52192b3c5fd31d05aad5e23b12
p3rf3ct1onis7 <lakewaters23@gmail.com>
2023-03-16 19:49:29 -0700

    I got commitment issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+* main - Entry point
+* @puts() - Writes a lin/string to the output.
+* Return: Always 0 (Success)
+*/
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
