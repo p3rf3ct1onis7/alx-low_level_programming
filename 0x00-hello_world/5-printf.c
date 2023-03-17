68da7eda70a5e793e513f4475ad8a2bd0109a350
p3rf3ct1onis7 <lakewaters23@gmail.com>
2023-03-16 19:47:44 -0700

    I got commitment issues

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+#include<stdio.h>
+/**
+ * main-Entry point
+ * Description: Prints the words "...with proper..."
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
