7e24aca7e66977e75eb0010487becaee855f3fdd
p3rf3ct1onis7 <lakewaters23@gmail.com>
2023-03-20 07:04:26 -0700

    I got commitment issues

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: Assigns random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+
+}
