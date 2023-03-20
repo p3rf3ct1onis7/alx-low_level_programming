3f43e76ff2cab3308b8c4ca02da8751572d23771
p3rf3ct1onis7 <lakewaters23@gmail.com>
2023-03-20 07:22:52 -0700

    I got commitment issues

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: Assigns random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
