36116b00ad5584f19509ddac9d9c1ee5f045f26d
p3rf3ct1onis7 <lakewaters23@gmail.com>
2023-03-20 07:42:11 -0700

    I got commitment issues

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: Assigns random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num < 57)
+			putchar(',');
+		if (num < 57)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
