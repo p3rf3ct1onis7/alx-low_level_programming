20c33d4d5ed67f111c4bdb3bf45f5703c8dd914e
p3rf3ct1onis7 <lakewaters23@gmail.com>
2023-03-21 05:58:58 -0700

    Insomniac

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * main - Entry - point
+ * print_alphabet - Entry point
  *
- * Return: Always 0 (success)
+ * Description: Prints the lowercase alphabets
+ *
+ * print_alphabet() prints the lowercase of alphabets
+ *
+ * Return: Void
  */
 void print_alphabet(void)
 {
