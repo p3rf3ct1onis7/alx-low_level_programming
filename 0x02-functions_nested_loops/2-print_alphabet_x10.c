cd80694311ddfb62a30aa5f411bb39e89c546db3
p3rf3ct1onis7 <lakewaters23@gmail.com>
2023-03-22 10:42:45 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: Prints 10 times alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{	
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			putchar(alpha);
+		}
+		putchar('\n');
+
+	}
+}
